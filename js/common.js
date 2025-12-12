581aa4a81d6c15f7dadd8d00d648275972ac3c25
niteoowl <xx.niteowl@gmail.com>
2025-12-12 21:48:04 +0900

    커밋

--- js/common.js
+++ js/common.js
@@ -184,6 +184,8 @@ const Utilsly = {
                 { name: "URL 인코더", icon: "link", path: "/tools/dev/url-encoder" },
                 { name: "Base64 인코더", icon: "package_2", path: "/tools/dev/base64-encoder" },
                 { name: "UUID 생성기", icon: "fingerprint", path: "/tools/dev/uuid-generator" },
+                { name: "인터넷 속도 측정", icon: "speed", path: "/tools/dev/speed-test" },
+                { name: "QR 코드 스캐너", icon: "qr_code_scanner", path: "/tools/dev/qr-scanner" },
             ]
         },
         {
